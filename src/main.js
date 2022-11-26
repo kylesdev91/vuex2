0ae1bc0699500f4cf1acee6497d8e9968a0f9984
Kyle Hernandez <kylesdev91@yahoo.com>
2022-11-26 10:00:36 -0600

    4

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App.vue";
+import { store } from "./store/store";
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  store: store,
+  el: "#app",
+  render: (h) => h(App),
+});
